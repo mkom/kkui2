2d14afcd8bac967c3afaa00a73d1c030400d1453
mkom <komar@upwardstech.com>
2018-08-13 14:57:04 +0700

    first commit

---
+++ app/filters.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App;
+
+/**
+ * Add <body> classes
+ */
+add_filter('body_class', function (array $classes) {
+    /** Add page slug if it doesn't exist */
+    if (is_single() || is_page() && !is_front_page()) {
+        if (!in_array(basename(get_permalink()), $classes)) {
+            $classes[] = basename(get_permalink());
+        }
+    }
+
+    /** Add class if sidebar is active */
+    if (display_sidebar()) {
+        $classes[] = 'sidebar-primary';
+    }
+
+    /** Clean up class names for custom templates */
+    $classes = array_map(function ($class) {
+        return preg_replace(['/-blade(-php)?$/', '/^page-template-views/'], '', $class);
+    }, $classes);
+
+    return array_filter($classes);
+});
+
+/**
+ * Add "… Continued" to the excerpt
+ */
+add_filter('excerpt_more', function () {
+    return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
+});
+
+/**
+ * Template Hierarchy should search for .blade.php files
+ */
+collect([
+    'index', '404', 'archive', 'author', 'category', 'tag', 'taxonomy', 'date', 'home',
+    'frontpage', 'page', 'paged', 'search', 'single', 'singular', 'attachment'
+])->map(function ($type) {
+    add_filter("{$type}_template_hierarchy", __NAMESPACE__.'\\filter_templates');
+});
+
+/**
+ * Render page using Blade
+ */
+add_filter('template_include', function ($template) {
+    $data = collect(get_body_class())->reduce(function ($data, $class) use ($template) {
+        return apply_filters("sage/template/{$class}/data", $data, $template);
+    }, []);
+    if ($template) {
+        echo template($template, $data);
+        return get_stylesheet_directory().'/index.php';
+    }
+    return $template;
+}, PHP_INT_MAX);
+
+/**
+ * Tell WordPress how to find the compiled path of comments.blade.php
+ */
+add_filter('comments_template', function ($comments_template) {
+    $comments_template = str_replace(
+        [get_stylesheet_directory(), get_template_directory()],
+        '',
+        $comments_template
+    );
+    return template_path(locate_template(["views/{$comments_template}", $comments_template]) ?: $comments_template);
+}, 100);
+
+/**
+ * remove  head scripts
+ */
+function remove_head_scripts() {
+    //remove Emoji, we are not using it
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_action( 'wp_head', 'wlwmanifest_link' );
+    remove_action( 'wp_head', 'wp_shortlink_wp_head' );
+    remove_action ( 'wp_head', 'rsd_link' );
+    //Remove WP generator from header, this is security potential.
+    remove_action('wp_head', 'wp_generator');
+    add_action('wp_footer', 'wp_print_scripts', 1);
+    add_action('wp_footer', 'wp_enqueue_scripts', 1);
+    add_action('wp_footer', 'wp_print_head_scripts', 1);
+    //Remove WP Rest API
+    remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+    remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
+}
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\remove_head_scripts', 100);
+/**
+ * Clean up output of stylesheet <link> tags
+ */
+function clean_style_tag( $input ) {
+    preg_match_all( "!<link rel='stylesheet'\s?(id='[^']+')?\s+href='(.*)' type='text/css' media='(.*)' />!", $input, $matches );
+    if ( empty( $matches[2] ) ) {
+        return $input;
+    }
+    // Only display media if it is meaningful
+    $media = $matches[3][0] !== '' && $matches[3][0] !== 'all' ? ' media="' . $matches[3][0] . '"' : '';
+    return '<link rel="stylesheet" href="' . $matches[2][0] . '"' . $media . '>' . "\n";
+}
+add_filter('style_loader_tag',  __NAMESPACE__ . '\\clean_style_tag');
+/**
+ * Clean up output of <script> tags
+ */
+function clean_script_tag( $input ) {
+    $input = str_replace( "type='text/javascript' ", '', $input );
+    return str_replace( "'", '"', $input );
+}
+add_filter('script_loader_tag',  __NAMESPACE__ . '\\clean_script_tag');
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header id="header" class="header">
+  <div class="container">
+    <div class="header__inner pt-5 pb-5 d-flex justify-content-between align-items-center">
+      <a class="brand d-flex align-items-center" title=" {{ get_bloginfo('name', 'display') }}" href="{{ home_url('/') }}" >
+        <img src="@asset('images/logo.png')" width="56" height="56" alt=" {{ get_bloginfo('name', 'display') }}" class="d-inline-block align-top mr-3">
+        {{ get_bloginfo('name', 'display') }}
+      </a>
+      <div class="socm d-flex align-items-center">
+        <a class="mr-3" href="https://instagram.com/" target="_blank" title="Instagram">
+          <img src="@asset('images/instagram.png')" width="24" height="24">
+        </a>
+        <a class="mr-3" href="https://facebook.com/" target="_blank" title="Facebook">
+          <img src="@asset('images/facebook.png')" width="24" height="24">
+        </a>
+        <a class="mr-3" href="https://twitter.com/" target="_blank" title="Twitter">
+          <img src="@asset('images/twitter.png')" width="24" height="20">
+        </a>
+      </div>
+    </div>
+  </div>
+</header>
