76edeb7a0b610cc79c2d9b4298b219bf748414a1
mkom <[komar@upwardstech.com]>
2018-09-17 16:17:17 +0700

    card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,9 +10,13 @@
     {!! get_search_form(false) !!}
   @endif
 
-  @while (have_posts()) @php the_post() @endphp
-    @include('partials.content-'.get_post_type())
-  @endwhile
+ <section class="section-row section-wrap">
+   <div class="row m-0">
+     @while (have_posts()) @php the_post() @endphp
+     @include('partials.content-'.get_post_type())
+     @endwhile
 
-  {!! get_the_posts_navigation() !!}
+     {!! get_the_posts_navigation() !!}
+   </div>
+ </section>
 @endsection
--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,9 +1,13 @@
-<article @php post_class() @endphp>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
+<div class="col-md-6 col-lg-4 p-0 mb-1 pr-1 card-content">
+  <div class="card h-100 no-radius">
+    <div class="card-body">
+      <header class="card-title">
+        <h5 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h5>
+        @include('partials/entry-meta')
+      </header>
+      <div class="entry-summary card-text">
+        @php the_excerpt() @endphp
+      </div>
+    </div>
   </div>
-</article>
+</div>
