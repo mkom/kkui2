909e0eb26a673aed032ba446d7741cb522cf58d2
mkom <[komar@upwardstech.com]>
2018-10-01 17:27:02 +0700

    update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,16 @@
-<footer class="content-info">
+<footer class="content-info footer pt-4 pb-4" id="footer">
   <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+    <hr>
+    <div class="d-md-flex justify-content-between align-items-center text-center">
+      <span>&copy; 2018</span>
+      <div class="socm  align-items-center d-md-flex">
+        <a class="mr-2" href="https://facebook.com/" target="_blank" title="Facebook">
+          <img src="@asset('images/facebook_2.png')" width="24" height="24">
+        </a>
+        <a class="mr-2" href="https://twitter.com/" target="_blank" title="Twitter">
+          <img src="@asset('images/twitter_2.png')" width="24" height="20">
+        </a>
+      </div>
+    </div>
   </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,24 +1,22 @@
 <header id="header" class="header">
   <div class="container">
     <div class="header__inner pt-3 pb-3  d-flex justify-content-between align-items-center">
+      <button data-toggle="modal" data-src="" data-target="#fullscreensearch" type="button" class="btn-header-search"><i class="fas fa-search"></i></button>
       <a class="brand d-flex align-items-center" title=" {{ get_bloginfo('name', 'display') }}" href="{{ home_url('/') }}" >
-        <img src="@asset('images/logo.png')" width="56" height="56" alt=" {{ get_bloginfo('name', 'display') }}" class="d-inline-block align-top mr-3">
+        <img src="@asset('images/logo.png')" width="24" height="24" alt=" {{ get_bloginfo('name', 'display') }}" class="d-inline-block align-top mr-1">
         {{ get_bloginfo('name', 'display') }}
       </a>
-      {{--@if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'container_class' =>'primary-nav d-none d-md-block', 'menu_class' => 'nav']) !!}
-      @endif--}}
-      <div class="socm d-flex align-items-center">
-        <a class="mr-3" href="https://instagram.com/" target="_blank" title="Instagram">
-          <img src="@asset('images/instagram_2.png')" width="24" height="24">
-        </a>
-        <a class="mr-3" href="https://facebook.com/" target="_blank" title="Facebook">
+      <div class="socm  align-items-center d-md-flex">
+        <a class="mr-2" href="https://facebook.com/" target="_blank" title="Facebook">
           <img src="@asset('images/facebook_2.png')" width="24" height="24">
         </a>
-        <a class="mr-3" href="https://twitter.com/" target="_blank" title="Twitter">
+        <a class="mr-2" href="https://twitter.com/" target="_blank" title="Twitter">
           <img src="@asset('images/twitter_2.png')" width="24" height="20">
         </a>
       </div>
+      {{--@if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'container_class' =>'primary-nav d-none d-md-block', 'menu_class' => 'nav']) !!}
+      @endif--}}
     </div>
     @if (has_nav_menu('primary_navigation'))
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'container_class' =>'primary-nav primary-m-nav p-1 mb-4', 'menu_class' => 'nav owl-carousel owl-theme menu-carousel', 'container_id' => 'container-m-nav']) !!}
