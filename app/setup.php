47f88cf323fb61e3b2b2405a0d8ec549161836cc
maskom <muh.komar@gmail.com>
2018-10-18 01:44:56 +0700

    update sidebar

--- app/setup.php
+++ app/setup.php
@@ -146,3 +146,26 @@ add_action('after_setup_theme', function () {
         return "<?= " . __NAMESPACE__ . "\\asset_path({$asset}); ?>";
     });
 });
+
+// DIY Popular Posts @ https://digwp.com/2016/03/diy-popular-posts/
+function popular_posts($post_id) {
+    $count_key = 'popular_posts';
+    $count = get_post_meta($post_id, $count_key, true);
+    if ($count == '') {
+        $count = 0;
+        delete_post_meta($post_id, $count_key);
+        add_post_meta($post_id, $count_key, '0');
+    } else {
+        $count++;
+        update_post_meta($post_id, $count_key, $count);
+    }
+}
+function track_posts($post_id) {
+    if (!is_single()) return;
+    if (empty($post_id)) {
+        global $post;
+        $post_id = $post->ID;
+    }
+    popular_posts($post_id);
+}
+add_action('wp_head',__NAMESPACE__ . '\\track_posts');
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,4 +1,35 @@
 <section class="popular-post widget mb-4 d-inline-block w-100">
-    <h3>Popular Post</h3>
+    <h3>{{ __('Popular Post', 'kkui') }}</h3>
+    @php
+        $args=array(
+            'meta_key'=>'popular_posts',
+            'orderby'=>'meta_value_num',
+             'order'=>'DESC',
+            'posts_per_page'=> 7,
+        );
+        $my_query = new WP_Query($args);
+    @endphp
+
+    @if($my_query->have_posts())
+        <ul>
+            @php $i=1; while ($my_query->have_posts()):  $my_query->the_post() @endphp
+                <li>
+                    <div class="d-flex justify-content-start align-items-center">
+                        <div class="pr-4 font-weight-light list-num">@php echo $i; @endphp</div>
+                        <div class="flex-column">
+                            <h4>
+                                <a href="{{ get_permalink() }}">
+                                    @php echo wp_trim_words( get_the_title(), 10, '...' ); @endphp
+                                </a>
+                            </h4>
+                            <div class="d-block small "> @include('partials/entry-meta')</div>
+                        </div>
+                    </div>
+
+                </li>
+            @php $i++; endwhile; @endphp
+        </ul>
+    @endif
 </section>
+@php wp_reset_query(); @endphp
 @php dynamic_sidebar('sidebar-primary') @endphp
